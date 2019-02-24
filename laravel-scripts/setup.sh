5f4691c6771277b0a89119b4ff5932525440712c
Arilson Gonçalves <arilson.senac.ads@gmail.com>
2019-02-24 16:03:37 -0300

    Update setup.sh

--- laravel-scripts/setup.sh
+++ laravel-scripts/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "Enter project directory"
+cd ../html/
+
 echo "Create laravel project"
-composer create-project --prefer-dist laravel/laravel <your-app>
+composer create-project --prefer-dist laravel/laravel condominio
 
 echo "Uploading Application container"
 docker-compose up -d
 
 echo "Apply permission to run laravel installation script"
-docker exec <your-app>-app chmod 777 /install-laravel.sh
+docker exec condominio-app chmod 777 /install-laravel.sh
 
 echo "Execute laravel installation"
-docker exec <your-app>-app /install-laravel.sh
+docker exec condominio-app /install-laravel.sh
